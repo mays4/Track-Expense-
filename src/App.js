4ffb6d01473bea24419bdb501da7f3472fea95ca
mays4 <maysalani84@yahoo.com>
2022-10-31 16:31:56 -0400

    first comment in Track app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import ExpenseItem from "./compontents/ExpenseItem";
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Let's get started</h2>
+    <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
