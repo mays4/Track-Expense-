fa635fc7e020b7d0ff51f1964e4f3e018f8aacd9
mays4 <maysalani84@yahoo.com>
2022-11-01 14:28:07 -0400

    split components

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import ExpenseItem from "./compontents/ExpenseItem";
 
 
 function App() {
+  const expenseData = [{
+    id:1,
+    title:"car insurance",
+    price:329.22,
+    date: new Date(2022,9,20)
+  },
+  {
+    id:2,
+    title:"pay rent",
+    price:1000.40,
+    date: new Date(2022,10,1)
+  }]
   return (
     <div className="App">
      <h2>Let's get started</h2>
-    <ExpenseItem></ExpenseItem>
+    <ExpenseItem  date = {expenseData[0].date}title={expenseData[0].title} price={expenseData[0].price}></ExpenseItem>
+    <ExpenseItem date={expenseData[1].date} title={expenseData[1].title} price={expenseData[1].price}></ExpenseItem>
     </div>
   );
 }
--- src/compontents/ExpenseItem.js
+++ src/compontents/ExpenseItem.js
@@ -1,3 +1,15 @@
-export default function ExpenseItem(){
-  return <h2>Expense Item!</h2>
+
+import './ExpenseItem.css'
+import ExpenseData from './ExpenseData';
+export default function ExpenseItem(props){
+  
+  return (
+  <div className='expense-item'>
+    <ExpenseData date={props.date}/>
+     <div className='expense-item__description'><h2>{props.title }</h2>
+     </div>
+     <div className='expense-item__price'>${props.price}</div>
+
+
+  </div>)
 }
